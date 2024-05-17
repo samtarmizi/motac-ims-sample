5de95f723aa81c1b56dd66596cc318ad82d19bd4
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2024-05-17 12:05:21 +0800

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,41 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // prepare production users data
+        /** 
+         *      Superadmin Admin MOTAC
+         *      superadmin@motac.gov.my
+         * */ 
+        User::create([
+            'name' => 'Superadmin Admin MOTAC',
+            'email' => 'superadmin@motac.gov.my',
+            'password' => Hash::make('password'),
+            'level' => 'admin',
+        ]);
+
+         /** 
+         *      Stockkeeper Admin MOTAC
+         *      stockkeeper@motac.gov.my
+         * */ 
+
+        User::create([
+            'name' => 'Stockkeeper Admin MOTAC',
+            'email' => 'stockeeper@motac.gov.my',
+            'password' => Hash::make('password'),
+            'level' => 'stockkeeper',
+        ]);
+
+         /** 
+         *      Customer Admin MOTAC
+         *      customer@motac.gov.my
+         * */ 
+        User::create([
+            'name' => 'Customer Admin MOTAC',
+            'email' => 'customer@motac.gov.my',
+            'password' => Hash::make('password'),
+            'level' => 'customer',
+        ]);
+        
     }
 }
