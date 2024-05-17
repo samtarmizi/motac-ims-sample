66ef04d2df4703c1b9f769460e6ca763a6707901
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2024-05-17 10:46:45 +0800

    add admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check.user.level' => \App\Http\Middleware\CheckUserLevelMiddleware::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
