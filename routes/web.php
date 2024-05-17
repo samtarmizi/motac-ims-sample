f62176b657b98609b808b664c68b740e4391bf24
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2024-05-17 10:36:59 +0800

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('check.user.level');
 
 Route::get('/admin/home', [App\Http\Controllers\Admin\HomeController::class, 'dashboard'])->name('admin.home');
